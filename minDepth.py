9d8dacf05eb997fdc17c16e12849ef77fc2c3fa2
wko1375 <38301560+wko1375@users.noreply.github.com>
2021-03-11 23:06:42 -0500

    Create minDepth.py

---
+++ minDepth.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        elif root.left == None and root.right == None:
+            return 1
+        elif root.left == None:
+            return 1 + self.minDepth(root.right)
+        elif root.right == None:
+            return 1 + self.minDepth(root.left)
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
